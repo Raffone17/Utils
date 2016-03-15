ed56e45cb81a23ab540ac7672ee0bc0dfa7b2641
Raffone17 <raffoespo@gmail.com>
2016-03-15 17:40:46 +0100

    Some new wicket

--- Java/NetBeansProjects/Mar15/src/main/java/com/siamobelli/mar15/HomePage.java
+++ Java/NetBeansProjects/Mar15/src/main/java/com/siamobelli/mar15/HomePage.java
@@ -3,6 +3,8 @@ package com.siamobelli.mar15;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.LabeledWebMarkupContainer;
+import org.apache.wicket.markup.repeater.RepeatingView;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -16,6 +18,17 @@ public class HomePage extends WebPage {
                 MenuPanel menu = new MenuPanel("menu");
                 add(doc);
                 add(menu);
+                
+                RepeatingView tab = new RepeatingView("riga");
+                tab.add(new Label(tab.newChildId(),"riga1"));
+                tab.add(new Label(tab.newChildId(),"riga2"));
+                tab.add(new Label(tab.newChildId(),"riga3"));
+                tab.add(new Label(tab.newChildId(),"riga4"));
+                tab.add(new Label(tab.newChildId(),"riga5"));
+                tab.add(new Label(tab.newChildId(),
+                        "chi ama la (|) metta una riga"));
+                
+                add(tab);
 
     }
 }
